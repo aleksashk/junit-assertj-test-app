e7850a0e05eb63e31a77109035a56b1ed326f147
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-22 23:12:11 +0300

    assertEquals 2 easy to add a new value

--- src/test/java/by/flameksandr/service/DataServiceTest.java
+++ src/test/java/by/flameksandr/service/DataServiceTest.java
@@ -11,11 +11,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DataServiceTest {
 
     @Test
-    void findMax() throws Exception {
+    void findMax(){
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 34, 4333);
+        List<Integer> numbers2 = Arrays.asList(67676761, 565652, 3454544, 699998787, 3435, 6, 7, 34, 4333);
         int max = DataService.findMax(numbers);
+        int max2 = DataService.findMax(numbers2);
 
         assertEquals(max, 4333);
+        assertEquals(max2, 699998787);
     }
 }
